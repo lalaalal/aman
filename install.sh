7d2128d13bffc2efbddb4e4c97d0ba50675fe7db
lalaalal <bontin0128@gmail.com>
2020-12-10 01:35:59 +0900

    .

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 chmod +x aman
 
 if [ ! -d "/usr/local/bin" ]; then
-    sudo mkdir -p /usr/local/bin
+    mkdir -p /usr/local/bin
 fi
-sudo cp ./aman /usr/local/bin/aman
+cp ./aman /usr/local/bin/aman
